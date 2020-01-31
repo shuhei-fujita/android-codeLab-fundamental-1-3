9b3bde942f33ceb9fe27e69e8f82dc8bcd97d73a
syuheifujita <mcslr772ed@gmail.com>
2020-02-01 04:26:24 +0900

    modify

--- app/src/main/java/com/example/android_codelab_fundamental_1_3/MainActivity.kt
+++ app/src/main/java/com/example/android_codelab_fundamental_1_3/MainActivity.kt
@@ -2,11 +2,36 @@ package com.example.android_codelab_fundamental_1_3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.button_view)
+        button.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val randomInt = (1..6).random()
+
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+
+        val drawableResource = when(randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
     }
 }
